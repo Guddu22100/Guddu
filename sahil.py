80cf84ca78ab2fcbdb99906a33db4deba04e378b
Guddu22100 <fakeooo107@gmail.com>
2025-05-17 12:03:49 +0530

    sahil.py

--- sahil.py
+++ sahil.py
@@ -11,12 +11,12 @@ from threading import Thread
 loop = asyncio.get_event_loop()
 
 # Bot token
-TOKEN = '7362289727:AAE6RWfho65aJUxMyOSdvxXVBndKGx4QZcc'
+TOKEN = '7769924593:AAGU9rcGMIrUAks6tU7Vry3XasziXH88_v4'
 bot = telebot.TeleBot(TOKEN)
 REQUEST_INTERVAL = 1
 
 # Admins list
-ADMIN_IDS = [2085082046]  # Replace with actual admin user IDs
+ADMIN_IDS = [6182507613]  # Replace with actual admin user IDs
 
 # File to store user information
 USERS_FILE = 'users.txt'
